6812425f0cf4f6ab39c5c902bca543e60827d5e7
ShutenOishi <be11the6change@gmail.com>
2026-09-18 16:55:18 +0900

    Phase 1: support patch-level option availability diagnostics

--- src/PgCliSharp/Internal/Localization/MessageKeys.cs
+++ src/PgCliSharp/Internal/Localization/MessageKeys.cs
@@ -8,6 +8,7 @@ internal static class MessageKeys
     internal const string ProcessExitedWithError = nameof(ProcessExitedWithError);
     internal const string ProcessTimedOut = nameof(ProcessTimedOut);
     internal const string OptionNotSupported = nameof(OptionNotSupported);
+    internal const string OptionRequiresExecutableVersion = nameof(OptionRequiresExecutableVersion);
     internal const string InvalidOptionCombination = nameof(InvalidOptionCombination);
     internal const string InvalidCompressionLevel = nameof(InvalidCompressionLevel);
     internal const string InvalidCompressionCombination = nameof(InvalidCompressionCombination);
@@ -23,6 +24,7 @@ internal static class MessageKeys
         ProcessExitedWithError,
         ProcessTimedOut,
         OptionNotSupported,
+        OptionRequiresExecutableVersion,
         InvalidOptionCombination,
         InvalidCompressionLevel,
         InvalidCompressionCombination,
