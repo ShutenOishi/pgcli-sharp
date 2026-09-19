b0b167e847940df0bd0644f3bc70c3711a355db8
ShutenOishi <be11the6change@gmail.com>
2026-09-19 09:28:18 +0900

    Phase 4: localize unsupported tool diagnostics

--- src/PgCliSharp/Internal/Localization/MessageKeys.cs
+++ src/PgCliSharp/Internal/Localization/MessageKeys.cs
@@ -7,7 +7,7 @@ internal static class MessageKeys
     internal const string ExecutableVersionOutputInvalid = nameof(ExecutableVersionOutputInvalid);
     internal const string ProcessExitedWithError = nameof(ProcessExitedWithError);
     internal const string ProcessTimedOut = nameof(ProcessTimedOut);
-    internal const string OptionNotSupported = nameof(OptionNotSupported);
+    internal const string OptionNotSupported = nameof(OptionNotSupported);\n    internal const string ToolNotSupported = nameof(ToolNotSupported);
     internal const string OptionRequiresExecutableVersion = nameof(OptionRequiresExecutableVersion);
     internal const string InvalidOptionCombination = nameof(InvalidOptionCombination);
     internal const string InvalidCompressionLevel = nameof(InvalidCompressionLevel);
@@ -33,7 +33,7 @@ internal static class MessageKeys
         ExecutableVersionOutputInvalid,
         ProcessExitedWithError,
         ProcessTimedOut,
-        OptionNotSupported,
+        OptionNotSupported,\n        ToolNotSupported,
         OptionRequiresExecutableVersion,
         InvalidOptionCombination,
         InvalidCompressionLevel,
